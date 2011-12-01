cf8ac1da372ac01de8afbf155aeacf727ac0b5c9
Casper Steinmann <casper.steinmann@gmail.com>
2011-12-01 14:55:59 +0900

    remove notice.

--- residue_charge.py
+++ residue_charge.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python
 """
-residue_charge.py - Determine the charge of residues using openbabel
-
-Copyright (C) 2010 by Casper Steinmann (casper.steinmann@gmail.com)
-
-For more information, see <http://pythonchem.blogspot.com/>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation version 2 of the License.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+residue_charge.py - Determine the charge of residues using Open Babel
 """
 import openbabel
 obConversion = openbabel.OBConversion()
